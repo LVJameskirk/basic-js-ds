1a614bc5d6e54c0ff8dbe6ddd1d24a62151b0403
LVJameskirk <LVJameskirk@gmail.com>
2023-12-25 14:06:40 +0300

    feat: add solutions for remove-from-lists, stack and queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.tail = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    const newList = new ListNode(value);
+
+    if (this.head === null) {
+      this.head = newList;
+      this.tail = newList;
+    } else {
+      this.tail.next = newList;
+      this.tail = newList;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.head === null) {
+      return undefined;
+    }
+
+    const value = this.head.value;
+    this.head = this.head.next;
+    
+    return value;
   }
 }
 
